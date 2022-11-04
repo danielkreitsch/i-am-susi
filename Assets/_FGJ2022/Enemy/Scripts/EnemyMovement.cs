e7df83770e6e765223e839d4038700ad6217816b
Daniel Kreitsch <daniel.kreitsch@creamcheesegames.com>
2022-11-04 23:02:16 +0100

    Create scripts

---
+++ Assets/_FGJ2022/Enemy/Scripts/EnemyMovement.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController characterController;
+
+    private void SetTarget()
+    {
+        
+    }
+}
