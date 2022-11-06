7851a387ccb2d04c675f89b0436690268ef407a6
Daniel Schulz <faulolio@gmail.com>
2022-11-06 01:53:07 +0100

    Tweak WinZone

--- Assets/_FGJ2022/_Common/Scripts/Effects/InstantiateParticlesEffect.cs
+++ Assets/_FGJ2022/_Common/Scripts/Effects/InstantiateParticlesEffect.cs
@@ -10,12 +10,18 @@ namespace Game.Effects {
 
         [Space]
         [SerializeField]
+        bool parentToContext = false;
+
+        [Space]
+        [SerializeField]
         bool overrideColor = false;
         [SerializeField, ConditionalField(nameof(overrideColor))]
         ParticleSystem.MinMaxGradient colorGradient = new();
 
         protected override void InvokeNow(GameObject context) {
-            var particlesInstance = Instantiate(particlesPrefab, context.transform.position, particlesPrefab.transform.rotation);
+            var particlesInstance = parentToContext
+                ? Instantiate(particlesPrefab, context.transform.position, particlesPrefab.transform.rotation, context.transform)
+                : Instantiate(particlesPrefab, context.transform.position, particlesPrefab.transform.rotation);
 
             if (overrideColor) {
                 var main = particlesInstance.main;
